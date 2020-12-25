65dd835b8f23efdd1b3a8faf44b9e183d72ef527
unknown <moody.kh.022@gmail.com>
2020-12-25 19:39:07 +0000

    second commit

---
+++ .anax/scaffold/postprocess.d/700_weather_package.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# anax/remserver
+#
+# Integrate the REM server onto an existing anax installation.
+#
+
+# Copy the configuration files
+# rsync -av vendor/anax/remserver/config ./
+
+# Copy the documentation
+# rsync -av vendor/anax/remserver/content/index.md ./content/remserver-api.md
+
+
+
+
+#!/usr/bin/env bash
+#
+# anax/anax-weather-module
+#
+
+# Copy config
+rsync -av vendor/muhamed/weather_ackage/config ./
+
+# Copy view
+
+rsync -av vendor/muhamed/weather_package/view ./
+
+# Copy src
+
+rsync -av vendor/muhamed/weather_package/src ./
+
+# Copy test
+
+rsync -av vendor/muhamed/weather_package/test ./
